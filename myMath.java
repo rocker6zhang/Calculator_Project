905359dc90a59bd56179bd6f7fb998ab4480a9d8
rocker6zhang <124797375@qq.com>
2017-05-24 14:28:25 +0800

    修补double计算溢出bug，将double替换为BigDecimal对对象

--- myMath.java
+++ myMath.java
@@ -1,47 +1,53 @@
 package Calculator_Project;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 
 
 public class myMath {
 	private double value;
 	private final int acy = 8;
-	
+	private MathContext context = new MathContext(acy*2);
+
 	//ÇóÖ¸Êý
 	public String power(String base, String index){
 		value = Math.pow(Double.valueOf(base), Double.valueOf(index));
 		return String.format("%."+acy+"f", value);
-		
+
 	}
 	//is plus
 	public String plus(String str, String str2){
-//		System.out.println(str+"**"+str2);
-		value = Double.valueOf(str) + Double.valueOf(str2);
-	
-		return String.format("%."+acy+"f", value);
+		BigDecimal b = new  BigDecimal(str,context);
+		BigDecimal b2 = new  BigDecimal(str2,context);
+		b = b.add(b2);
+		return String.format("%."+acy+"f", b );
 		//String.valueOf(value);
 	}
-	
+
 	//is less
 	public String less(String str, String str2){
-		value = Double.valueOf(str) - Double.valueOf(str2);
-		return String.format("%."+acy+"f", value);
+		BigDecimal b = new  BigDecimal(str);
+		BigDecimal b2 = new  BigDecimal(str2);
+		b = b.subtract(b2);
+		return String.format("%."+acy+"f", b);
 	}
-	
+
 	//is multiply
 	public String multiply(String str, String str2){
-		 BigDecimal b = new  BigDecimal(str);
-		 BigDecimal b2 = new  BigDecimal(str2);
-		 b = b.multiply(b2);
+
+		BigDecimal b = new  BigDecimal(str,context);
+		BigDecimal b2 = new  BigDecimal(str2,context);
+		b = b.multiply(b2);
 		return String.format("%."+acy+"f", b);
-		
+
 	}
-	
+
 	//is except
 	public String exsept(String str, String str2){
-//		System.out.println("//"+str+"**"+str2);
-		
-//		System.out.println(value);
-		return String.format("%."+acy+"f", Double.valueOf(str) / Double.valueOf(str2) );
+		//System.out.println(str+"*****"+str2);
+		BigDecimal b = new  BigDecimal(str,context);
+		BigDecimal b2 = new  BigDecimal(str2,context);
+		b = b.divide(b2,context);
+		return String.format("%."+acy+"f", b );
 	}
 }
